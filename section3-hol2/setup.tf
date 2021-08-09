acfec35c0b83e572ff1b271043fc96604c42fc4d
DudyShavit <shavit.dudy@gmail.com>
2021-08-09 13:29:26 +0300

    Update setup.tf

--- section3-hol2/setup.tf
+++ section3-hol2/setup.tf
@@ -2,13 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-#Create key-pair for logging into EC2 in us-east-1
-resource "aws_key_pair" "webserver-key" {
-  key_name   = "webserver-key"
-  public_key = file("~/.ssh/id_rsa.pub")
-}
-
-
 #Get Linux AMI ID using SSM Parameter endpoint in us-east-1
 data "aws_ssm_parameter" "webserver-ami" {
   name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
